057d86c9a3e97a45f94eaa45f4d4b2f8223cd7bc
Tobias Roeser <le.petit.fou@web.de>
2019-02-13 09:33:36 +0100

    Moved and added missing scripts

---
+++ scripts/build-prereq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+publishLocal() {
+  cd "${TRAVIS_BUILD_DIR}"
+  local REPO="https://github.com/woq-blended/$1.git"
+  local REPODIR="${TRAVIS_BUILD_DIR}/$1"
+  rm -Rf ${REPODIR}
+  echo "Cloning ${REPO}..."
+  git clone --branch master --depth 1 \
+    "$REPO" "$REPODIR"
+  cd "$REPODIR"
+  echo "Building and (local) publishing ${REPO}..."
+  sbt publishLocal
+  cd "${TRAVIS_BUILD_DIR}"
+}
+
+for repo in "$@"; do
+  publishLocal "$repo"
+done
