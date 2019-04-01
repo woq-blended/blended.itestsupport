e0dc0c29e75eb86647f93889d8686d87160d6278
Tobias Roeser <le.petit.fou@web.de>
2019-04-01 09:48:07 +0200

    Use exported dockerJava dependency from blended project

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,9 @@ import sbt._
 
 object Dependencies extends blended.sbt.Dependencies {
 
-  private[this] val dockerJavaVersion = "3.0.13"
-
   val akktHttpTestkit = akka_Http("http-testkit")
 
   val commonsCompress = "org.apache.commons" % "commons-compress" % "1.13"
-
-  val dockerJava = "com.github.docker-java" % "docker-java" % dockerJavaVersion
 
 }
 
