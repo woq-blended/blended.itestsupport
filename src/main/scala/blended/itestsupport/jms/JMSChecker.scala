4d5e4ca9267139cb993c4260d64e34fa690f8bb2
Andreas Gies <andreas@wayofquality.de>
2019-09-24 11:37:15 +0200

    Removing Camel Dependencies

--- src/main/scala/blended/itestsupport/jms/JMSChecker.scala
+++ src/main/scala/blended/itestsupport/jms/JMSChecker.scala
@@ -2,7 +2,6 @@ package blended.itestsupport.jms
 
 import java.util.concurrent.atomic.AtomicBoolean
 
-import javax.jms.ConnectionFactory
 import akka.actor._
 import blended.itestsupport.condition.{AsyncChecker, AsyncCondition}
 import blended.jms.utils._
@@ -13,21 +12,15 @@ import scala.concurrent.duration._
 import scala.util.{Failure, Success, Try}
 
 object JMSAvailableCondition {
-  def apply(cf: ConnectionFactory, t: Option[FiniteDuration] = None)(implicit system: ActorSystem) =
+  def apply(cf: IdAwareConnectionFactory, t: Option[FiniteDuration] = None)(implicit system: ActorSystem) =
     AsyncCondition(Props(JMSChecker(cf)), s"JMSAvailableCondition($cf)", t)
 }
 
 private[jms] object JMSChecker {
-  def apply(cf: ConnectionFactory)(implicit system : ActorSystem) = new JMSChecker(cf)
+  def apply(cf: IdAwareConnectionFactory) = new JMSChecker(cf)
 }
 
-private[jms] class JMSChecker(cf: ConnectionFactory)(implicit val system : ActorSystem) extends AsyncChecker {
-
-  private val connCfg : ConnectionConfig = BlendedJMSConnectionConfig.defaultConfig.copy(
-    vendor = "jms", provider = "check", keepAliveEnabled = false
-  )
-
-  private val idCf : IdAwareConnectionFactory = new SimpleIdAwareConnectionFactory(connCfg, cf, None)
+private[jms] class JMSChecker(cf: IdAwareConnectionFactory) extends AsyncChecker {
 
   private val log : Logger = Logger[JMSChecker]
   var connected: AtomicBoolean = new AtomicBoolean(false)
@@ -44,7 +37,7 @@ private[jms] class JMSChecker(cf: ConnectionFactory)(implicit val system : Actor
     if ((!connected.get()) && (!connecting.get())) {
       connecting.set(true)
 
-      Try { idCf.createConnection() } match {
+      Try { cf.createConnection() } match {
         case Success(_) =>
           connected.set(true)
         case Failure(t) =>
--- src/test/scala/blended/itestsupport/jms/JMSConditionAvailableSpec.scala
+++ src/test/scala/blended/itestsupport/jms/JMSConditionAvailableSpec.scala
@@ -16,22 +16,31 @@
 
 package blended.itestsupport.jms
 
+import akka.actor.ActorSystem
 import akka.testkit.{TestActorRef, TestProbe}
 import blended.itestsupport.condition.ConditionActor
 import blended.itestsupport.condition.ConditionActor.CheckCondition
 import blended.itestsupport.condition.ConditionActor.ConditionCheckResult
+import blended.jms.utils.{IdAwareConnectionFactory, SimpleIdAwareConnectionFactory}
 import blended.testsupport.TestActorSys
 import org.apache.activemq.ActiveMQConnectionFactory
+import scala.concurrent.duration._
 
 class JMSConditionAvailableSpec extends AbstractJMSSpec {
 
   "The JMS Available Condition" should {
 
     "fail if no connection can be made" in TestActorSys { testkit =>
-      implicit val system = testkit.system
-      val probe = TestProbe()
+      implicit val system : ActorSystem = testkit.system
+      val probe : TestProbe = TestProbe()
 
-      val cf = new ActiveMQConnectionFactory("vm://foo?create=false")
+      val cf : IdAwareConnectionFactory = SimpleIdAwareConnectionFactory(
+        vendor = "amq",
+        provider = "amq",
+        cf = new ActiveMQConnectionFactory("vm://foo?create=false"),
+        clientId = "test",
+        minReconnect = 1.second
+      )
       val condition = JMSAvailableCondition(cf)
 
       val checker = TestActorRef(ConditionActor.props(cond = condition))
@@ -41,10 +50,17 @@ class JMSConditionAvailableSpec extends AbstractJMSSpec {
     }
 
     "succeed if a connection can be made" in TestActorSys { testkit =>
-      implicit val system = testkit.system
-      val probe = TestProbe()
+      implicit val system : ActorSystem = testkit.system
+      val probe : TestProbe = TestProbe()
 
-      val cf = new ActiveMQConnectionFactory("vm://blended?create=false")
+      val cf : IdAwareConnectionFactory = SimpleIdAwareConnectionFactory(
+        vendor = "amq",
+        provider = "amq",
+        cf = new ActiveMQConnectionFactory("vm://blended?create=false"),
+        clientId = "test",
+        minReconnect = 1.second
+      )
+
       val condition = JMSAvailableCondition(cf)
 
       val checker = TestActorRef(ConditionActor.props(cond = condition))
